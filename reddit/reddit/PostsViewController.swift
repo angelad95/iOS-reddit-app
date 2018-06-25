d9ad67cf7d137a46d79e6fdc1d53d098d7c2dfef
angelad95 <38287344+angelad95@users.noreply.github.com>
2018-06-25 17:01:23 -0400

    Add files via upload

---
+++ reddit/reddit/PostsViewController.swift
@@ -0,0 +1,68 @@
+//
+//  PostsViewController.swift
+//  reddit
+//
+//  Created by Dawkins, Angela on 6/25/18.
+//  Copyright © 2018 Dawkins, Angela. All rights reserved.
+//
+
+import UIKit
+
+class PostsViewController: UITableViewController, UISearchResultsUpdating {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        loadSearchBar()
+
+    }
+    //MARK: Table view data source
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if searchController.isActive == true {
+            return searchResults.count
+        } else {
+            return model.featuredPosters.count
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "postsCell", for: indexPath)
+        
+        if searchController.isActive == true {
+            cell.textLabel?.text = searchResults[indexPath.row]["description"] as? String
+        } else {
+            cell.textLabel?.text = model.featuredPosters[indexPath.row]["description"]
+        }
+        return cell
+    }
+    //MARK - Search Controller
+    let searchController = UISearchController(searchResultsController: nil)
+    var searchResults = [AnyObject]()
+    
+    //MARK: UISearchResults Protocol
+    func loadSearchBar(){
+        searchController.searchResultsUpdater = self
+        tableView.tableHeaderView = searchController.searchBar
+        self.definesPresentationContext = false
+        searchController.dimsBackgroundDuringPresentation = false
+    }
+    
+    func updateSearchResults(for searchController: UISearchController){
+        //Get data as array
+        //filter data
+        //return search results
+        let predicate = NSPredicate(format: "description contains[cd] %@", searchController.searchBar.text!)
+        let filteredResults = (model.featuredPosters as NSArray).filtered(using: predicate)
+        
+        searchResults = filteredResults as [AnyObject]
+        tableView.reloadData()
+        
+        //print(filteredResults)
+        //print("Number of results: \(filteredResults.count)")
+        
+    }
+
+}
---
+++ reddit/reddit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  reddit
+//
+//  Created by Dawkins, Angela on 6/25/18.
+//  Copyright © 2018 Dawkins, Angela. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
